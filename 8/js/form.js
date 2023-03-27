9f7d446dee881bfe95f9b47d79d574aa3e2e6769
Keks <keks@htmlacademy.ru>
2023-03-27 19:22:16 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/form.js
@@ -0,0 +1,97 @@
+import { isEscapeKey } from './util.js';
+
+const HASHTAG_REGEXP = /^#[a-zа-яё0-9]{1,19}$/i;
+const MAX_HASHTAG_COUNT = 5;
+
+const imgUploadOverlay = document.querySelector('.img-upload__overlay');
+const imgUploadFile = document.querySelector('#upload-file');
+const imgUploadCancel = document.querySelector('#upload-cancel');
+const imgUploadForm = document.querySelector('.img-upload__form');
+const hashtagField = imgUploadForm.querySelector('.text__hashtags');
+const commentField = imgUploadForm.querySelector('.text__description');
+
+const isInFocusField = () => document.activeElement === hashtagField || document.activeElement === commentField;
+
+const onDocumentKeydown = (evt) => {
+  if (isEscapeKey(evt) && !isInFocusField()) {
+    evt.preventDefault();
+    closeUploadOverlay();
+  }
+};
+
+const onClickcloseUploadOverlay = (evt) => {
+  evt.preventDefault();
+  closeUploadOverlay();
+};
+
+function closeUploadOverlay() {
+  imgUploadOverlay.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+  imgUploadCancel.removeEventListener('click', onClickcloseUploadOverlay);
+  imgUploadForm.reset();
+}
+
+const openImgUploadForm = () => {
+  imgUploadOverlay.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+  imgUploadCancel.addEventListener('click', onClickcloseUploadOverlay);
+};
+
+imgUploadFile.addEventListener('change', openImgUploadForm);
+
+const pristine = new Pristine(imgUploadForm, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper__error'
+});
+
+const isValidHashtag = (hashtag) => HASHTAG_REGEXP.test(hashtag);
+
+const isValidLengthHashtag = (hashtags) => hashtags.length <= MAX_HASHTAG_COUNT;
+
+const isUniqueHashtag = (hashtags) => {
+  const lowerCaseHashtags = hashtags.map((hashtag) => hashtag.toLowerCase());
+  // return lowerCaseHashtags.length === new Set(lowerCaseHashtags).size;
+  const uniqHashtag = [];
+  for (let i = 0; i < lowerCaseHashtags.length; i++) {
+    if (uniqHashtag.includes(lowerCaseHashtags[i])) {
+      return false;
+    } else {
+      uniqHashtag.push(lowerCaseHashtags[i]);
+    }
+  }
+  return true;
+};
+
+const validateHashtag = (value) => {
+  const hashtags = value
+    .trim()
+    .split(' ')
+    .filter((hashtag) => hashtag.trim().length);
+  return hashtags.every(isValidHashtag) && isValidLengthHashtag(hashtags) && isUniqueHashtag(hashtags);
+};
+
+pristine.addValidator(
+  hashtagField,
+  validateHashtag,
+  'Неправильное заполнение поля'
+);
+
+const validateComment = (value) => value.length <= 140;
+
+pristine.addValidator(
+  commentField,
+  validateComment,
+  'До 140 символов'
+);
+
+const onFormSubmit = (evt) => {
+  evt.preventDefault();
+  if (pristine.validate()) {
+    imgUploadForm.submit();
+  }
+};
+
+imgUploadForm.addEventListener('submit', onFormSubmit);
---
+++ 8/js/thumbnail.js
@@ -0,0 +1,24 @@
+import { openBigPicture } from './photo-modal.js';
+
+const photoListElement = document.querySelector('.pictures');
+const photoTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const photoListFragment = document.createDocumentFragment();
+
+const renderPhoto = (photo) => {
+  photo.forEach((element) => {
+    const { url, likes, comments, description } = element;
+    const photoElement = photoTemplate.cloneNode(true);
+    photoElement.querySelector('.picture__img').src = url;
+    photoElement.querySelector('.picture__img').alt = description;
+    photoElement.querySelector('.picture__likes').textContent = likes;
+    photoElement.querySelector('.picture__comments').textContent = comments.length;
+    photoListFragment.append(photoElement);
+    openBigPicture(photoElement, element);
+  });
+  photoListElement.append(photoListFragment);
+};
+
+export { renderPhoto };
