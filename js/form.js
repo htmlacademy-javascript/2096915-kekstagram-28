decc382d0e2048324915c99c73f800cb2294b8be
Vitaliy Pancyrev <vpancyrev99@gmail.com>
2023-03-27 19:20:34 +0300

    add: validation form

--- js/form.js
+++ js/form.js
@@ -1,21 +1,34 @@
 import { isEscapeKey } from './util.js';
 
+const HASHTAG_REGEXP = /^#[a-zа-яё0-9]{1,19}$/i;
+const MAX_HASHTAG_COUNT = 5;
+
 const imgUploadOverlay = document.querySelector('.img-upload__overlay');
 const imgUploadFile = document.querySelector('#upload-file');
 const imgUploadCancel = document.querySelector('#upload-cancel');
 const imgUploadForm = document.querySelector('.img-upload__form');
+const hashtagField = imgUploadForm.querySelector('.text__hashtags');
+const commentField = imgUploadForm.querySelector('.text__description');
+
+const isInFocusField = () => document.activeElement === hashtagField || document.activeElement === commentField;
 
 const onDocumentKeydown = (evt) => {
-  if (isEscapeKey(evt)) {
+  if (isEscapeKey(evt) && !isInFocusField()) {
     evt.preventDefault();
     closeUploadOverlay();
   }
+};
+
+const onClickcloseUploadOverlay = (evt) => {
+  evt.preventDefault();
+  closeUploadOverlay();
 };
 
 function closeUploadOverlay() {
   imgUploadOverlay.classList.add('hidden');
   document.body.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
+  imgUploadCancel.removeEventListener('click', onClickcloseUploadOverlay);
   imgUploadForm.reset();
 }
 
@@ -23,50 +36,53 @@ const openImgUploadForm = () => {
   imgUploadOverlay.classList.remove('hidden');
   document.body.classList.add('modal-open');
   document.addEventListener('keydown', onDocumentKeydown);
+  imgUploadCancel.addEventListener('click', onClickcloseUploadOverlay);
 };
 
 imgUploadFile.addEventListener('change', openImgUploadForm);
 
-imgUploadCancel.addEventListener('click', () => {
-  closeUploadOverlay();
-});
-
 const pristine = new Pristine(imgUploadForm, {
   classTo: 'img-upload__field-wrapper',
-  // errorClass: 'form__item--invalid',
   errorTextParent: 'img-upload__field-wrapper',
-  // errorTextTag: 'span',
   errorTextClass: 'img-upload__field-wrapper__error'
 });
 
-// const validateLengthHashtag = (value) => value.length <= 20;
+const isValidHashtag = (hashtag) => HASHTAG_REGEXP.test(hashtag);
 
-// const validateWritingHashtag = (value) => {
-//   const regexp = /^#[a-zа-яё0-9]{1,19}$/i;
-//   return value.test(regexp);
-// };
+const isValidLengthHashtag = (hashtags) => hashtags.length <= MAX_HASHTAG_COUNT;
 
-// const validateHashtag = (value) => validateLengthHashtag(value) && validateWritingHashtag(value);
+const isUniqueHashtag = (hashtags) => {
+  const lowerCaseHashtags = hashtags.map((hashtag) => hashtag.toLowerCase());
+  // return lowerCaseHashtags.length === new Set(lowerCaseHashtags).size;
+  const uniqHashtag = [];
+  for (let i = 0; i < lowerCaseHashtags.length; i++) {
+    if (uniqHashtag.includes(lowerCaseHashtags[i])) {
+      return false;
+    } else {
+      uniqHashtag.push(lowerCaseHashtags[i]);
+    }
+  }
+  return true;
+};
 
-// хэш-тег начинается с символа # (решётка);
-// строка после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т. д.;
-// хеш-тег не может состоять только из одной решётки;
-// хэш-теги нечувствительны к регистру: #ХэшТег и #хэштег считаются одним и тем же тегом;
-// хэш-теги разделяются пробелами;
-// один и тот же хэш-тег не может быть использован дважды;
-// нельзя указать больше пяти хэш-тегов;
-// если фокус находится в поле ввода хэш-тега, нажатие на Esc не должно приводить к закрытию формы редактирования изображения.
-// pristine.addValidator(
-//   imgUploadForm.querySelector('.text__hashtags'),
-//   validateHashtag,
-//   'Что-то не то'
-// );
+const validateHashtag = (value) => {
+  const hashtags = value
+    .trim()
+    .split(' ')
+    .filter((hashtag) => hashtag.trim().length);
+  return hashtags.every(isValidHashtag) && isValidLengthHashtag(hashtags) && isUniqueHashtag(hashtags);
+};
+
+pristine.addValidator(
+  hashtagField,
+  validateHashtag,
+  'Неправильное заполнение поля'
+);
 
 const validateComment = (value) => value.length <= 140;
 
-// если фокус находится в поле ввода комментария, нажатие на Esc не должно приводить к закрытию формы редактирования изображения.
 pristine.addValidator(
-  imgUploadForm.querySelector('.text__description'),
+  commentField,
   validateComment,
   'До 140 символов'
 );
@@ -79,13 +95,3 @@ const onFormSubmit = (evt) => {
 };
 
 imgUploadForm.addEventListener('submit', onFormSubmit);
-
-
-// Сброс значений
-// const resetValue = () => {
-//   document.querySelector('input[type=file]').value = '';
-//   // document.querySelector('.text__hashtags').value = '';
-//   // document.querySelector('.text__description').value = '';
-//   // document.querySelector('.effects__preview').value = '';
-//   // document.querySelector('.effect-level__value').value = '';
-// };
