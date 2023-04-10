f8b0e567d3adb9bb1444f4213d6ed4dec5bcb69a
Vitaliy Pancyrev <vpancyrev99@gmail.com>
2023-04-10 23:02:06 +0300

    add: filter.js

--- js/main.js
+++ js/main.js
@@ -2,9 +2,17 @@ import { renderPhoto } from './thumbnail.js';
 import { resetForm } from './form.js';
 import { showAlert } from './util.js';
 import { getData } from './api.js';
+import { debounce } from './util.js';
+import { uploadPictures } from './filter.js';
+
+const RERENDER_DELAY = 500;
 
 getData()
-  .then((thumbnail) => renderPhoto(thumbnail))
+  .then((thumbnail) => {
+    const debounceRenderPhoto = debounce(renderPhoto, RERENDER_DELAY);
+    renderPhoto(thumbnail);
+    uploadPictures(thumbnail, debounceRenderPhoto);
+  })
   .catch((err) => showAlert(err.message));
 
 resetForm();
